51230e969349ff6d53f378232966bdce4a45dc7f
Mark Lamson <lamson_mark@yahoo.com>
2015-01-19 14:11:07 -0500

    done

--- main.js
+++ main.js
@@ -8,33 +8,71 @@
 
 
 
-var sec = 50;
-var min = 59;
-var hour = 23;
+var sec = 5;
+var secTens = 5;
+var min = 9;
+var minTens = 5;
+var hour = 3;
+var hourTens = 2;
 
 var myClock = setInterval(clockCount, 1000);
 	
 function clockCount(){
 	sec++;
 
-if(sec === 60){
-	min++;
+if(sec === 10){
+	secTens++;
 	sec = 0;
 }
 
 document.getElementById("secDisplay").innerHTML = (sec);
 
-if(min === 60){
-		hour++;
-		min = 0;
+if(secTens === 6){
+	min++;
+	secTens = 0;
+	sec = 0;
 }
+
+document.getElementById("secTensDisplay").innerHTML = (secTens);
+
+if(min === 10){
+	minTens++;
+	min = 0;
+	secTens = 0;
+	sec = 0;
+}
+
 document.getElementById("minDisplay").innerHTML = (min);
-if(hour === 24){
+
+if(minTens === 6){
+	hour++;
+	minTens = 0;
+	min = 0;
+	secTens = 0;
+	sec = 0;
+}
+
+document.getElementById("minTensDisplay").innerHTML = (minTens);
+
+if(hour === 4){
+		hourTens++
 		hour = 0;
+		minTens = 0;
 		min = 0;
+		secTens = 0;
 		sec = 0;
 }
-	document.getElementById("hourDisplay").innerHTML = (hour);
+document.getElementById("hourDisplay").innerHTML = (hour);
+
+if(hourTens === 3){
+		hourTens = 0;
+		hour = 0;
+		minTens = 0;
+		min = 0;
+		secTens = 0;
+		sec = 0;
+}
+	document.getElementById("hourTensDisplay").innerHTML = (hourTens);
 	
 	
 
